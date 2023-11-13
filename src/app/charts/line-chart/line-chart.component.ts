16e0c4d540261eb490c80dc85a8fba81390e5487
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-11-13 12:08:59 +0100

    transfer the data call to the parent component, graph working and data displaying (medals, atheletes, etc)

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -9,14 +9,8 @@ import { Participation } from 'src/app/core/models/Participation';
   styleUrls: ['./line-chart.component.scss'],
 })
 export class LineChartComponent implements OnInit {
-  @Input() data: Array<OlympicCountry> = [];
+  @Input() chartData: Array<Participation> = [];
   @Input() country: string = '';
-
-  private participationData: Array<any> = [];
-
-  private numberOfEntries: number = 0;
-  private numberOfMedals: number = 0;
-  private numberofAthletes: number = 0;
 
   public lineChart: any;
 
@@ -25,14 +19,8 @@ export class LineChartComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (this.data && this.data.length) {
-      this.extractParticipationData();
-      if (this.participationData.length > 0) {
-        this.calculateStatistics();
-        this.setUpLineChart();
-        console.log(this.participationData);
-      }
-    }
+    this.setUpLineChart();
+    console.log('fetchedData : ', this.chartData);
   }
 
   /* Calculation for medal count */
@@ -45,50 +33,14 @@ export class LineChartComponent implements OnInit {
       )
   ); */
 
-  private extractParticipationData() {
-    // Check if the 'country' input is provided and not an empty string
-    if (this.country && this.country.trim() !== '') {
-      // Filter data based on the 'country' input value
-      const selectedCountry = this.data.find(
-        (item) => item.country === this.country
-      );
-
-      // Check if the selected country is found
-      if (selectedCountry) {
-        this.participationData = selectedCountry.participations;
-      } else {
-        console.warn(`Country '${this.country}' not found in the data.`);
-        this.participationData = [];
-      }
-    } else {
-      console.warn('Country input is not provided or is an empty string.');
-      this.participationData = [];
-    }
-  }
-
-  private calculateStatistics() {
-    this.numberOfEntries = this.participationData.length;
-    this.numberOfMedals = this.participationData.reduce(
-      (totalMedals, participation) => totalMedals + participation.medalsCount,
-      0
-    );
-    this.numberofAthletes = this.participationData.reduce(
-      (totalAthletes, participation) =>
-        totalAthletes + participation.athleteCount,
-      0
-    );
-  }
-
   private setUpLineChart() {
     this.lineChart = new Chart('line-chart-canvas', {
       type: 'line',
       data: {
-        labels: this.participationData.map(
-          (participation) => participation.year
-        ),
+        labels: this.chartData.map((participation) => participation.year),
         datasets: [
           {
-            data: this.participationData.map(
+            data: this.chartData.map(
               (participation) => participation.medalsCount
             ),
           },
--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap, catchError } from 'rxjs';
+import { OlympicCountry } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,11 +10,12 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-  /* public numberOfEntries: number;
-  public numberOfAthletes: number;
-  public numberOfMedals: number; */
   public countryName: string = '';
+  public numberOfEntries: number = 0;
+  public numberOfMedals: number = 0;
+  public numberofAthletes: number = 0;
 
+  public participationData: Array<any> = [];
   public olympics$: Observable<any> = of(null);
 
   constructor(
@@ -23,10 +25,61 @@ export class DetailsComponent implements OnInit {
 
   ngOnInit() {
     this.countryName = this.route.snapshot.params['country'];
-    this.olympics$ = this.olympicService.getOlympics();
-    console.log(this.olympics$);
-    /* this.numberOfEntries = 15;
-    this.numberOfMedals = 40;
-    this.numberOfAthletes = 109; */
+    this.olympics$ = this.olympicService.getOlympics().pipe(
+      catchError((error) => {
+        console.error('Error fetching Olympic data:', error);
+        return of(null); // Return an empty observable or handle the error as appropriate
+      }),
+      tap((formattedData) => {
+        if (formattedData) {
+          this.participationData = this.extractParticipationData(formattedData);
+          const { numberOfEntries, numberOfMedals, numberofAthletes } =
+            this.calculateStatistics(this.participationData);
+          console.log(this.participationData);
+
+          // Use the values directly within this block
+          this.numberOfEntries = numberOfEntries;
+          this.numberOfMedals = numberOfMedals;
+          this.numberofAthletes = numberofAthletes;
+        }
+      })
+    );
+  }
+
+  private extractParticipationData(data: any) {
+    // Check if the 'country' input is provided and not an empty string
+    if (this.countryName && this.countryName.trim() !== '') {
+      // Filter data based on the 'country' input value
+      const selectedCountry = data.find(
+        (item: any) => item.country === this.countryName
+      );
+
+      // Check if the selected country is found
+      if (selectedCountry) {
+        return selectedCountry.participations;
+      } else {
+        console.warn(`Country '${this.countryName}' not found in the data.`);
+        this.participationData = [];
+      }
+    } else {
+      console.warn('Country input is not provided or is an empty string.');
+      this.participationData = [];
+    }
+  }
+
+  private calculateStatistics(data: any) {
+    const numberOfEntries = data.length;
+    console.log(data);
+    const numberOfMedals = data.reduce(
+      (totalMedals: any, participation: any) =>
+        totalMedals + participation.medalsCount,
+      0
+    );
+    const numberofAthletes = data.reduce(
+      (totalAthletes: any, participation: any) =>
+        totalAthletes + participation.athleteCount,
+      0
+    );
+    return { numberOfEntries, numberOfMedals, numberofAthletes };
   }
 }
