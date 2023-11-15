3b52bd740d6d9236fc05e4ba9454a24ec6865644
BaptLab <baptiste.labaune@gmail.com>
2023-11-15 16:10:41 +0100

    final code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicCountry } from 'src/app/core/models/Olympic';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,15 @@ import { OlympicCountry } from 'src/app/core/models/Olympic';
 })
 export class HomeComponent implements OnInit {
   // Observable to hold Olympic data, initialized with an observable of an empty array
-  public olympics$ = this.olympicService.getOlympics();
+  public olympics$ = this.olympicService.getOlympics().pipe(
+    catchError((error) => {
+      // Handle the error here, you can log it or show a user-friendly message
+      console.error('Error fetching Olympic data:', error);
+      // Return an empty observable to keep the stream alive
+      return EMPTY;
+    })
+  );
+
   public dataInput: OlympicCountry[] = [];
 
   // Inject OlympicService into the constructor
